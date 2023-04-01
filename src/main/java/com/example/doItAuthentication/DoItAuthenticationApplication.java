4423e4fb1f4c49271bf0b59c97fa568218fba7a4
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2023-04-01 10:23:06 +0900

    init

---
+++ src/main/java/com/example/doItAuthentication/DoItAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.doItAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoItAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doItAuthentication/DoItAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doItAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoItAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
